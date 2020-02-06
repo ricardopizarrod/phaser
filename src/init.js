3f4e9533a38182f024ffb0bdf49b6152e7bbf621
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-02-06 11:03:07 -0300

    v.0.0.2

--- src/init.js
+++ src/init.js
@@ -7,15 +7,33 @@ const config = {
         preload: preload,
         create: create,
         update: update
+    },
+    physics:{
+        default: "arcade",
+        arcade: {
+            gravity: {
+                y: 500
+            }
+        }
+
     }
 }
  var gamr = new Phaser.Game(config);
 function preload(){
-    console.log("Soy preload")
+    this.load.image("pajaro", "./assets/bird.png");
 }
 function create(){
-    console.log("Soy create")
+    this.pajaro= this.physics.add.image(80, 100, "pajaro");
+    this.pajaro.setScale(2);
+    this.pajaro.flipX = true;
+    this.pajaro.setOrigin(0.5);
+    //Propiedades Fisicas
+    this.pajaro.setCollideWorldBounds(true);
+    this.pajaro.setBounce(0.3);
+    this.pajaro.setVelocity(50,0);
+
 }
 function update(time, delta){
-    console.log(delta)
+    //this.pajaro.x++;
+ 
 }
